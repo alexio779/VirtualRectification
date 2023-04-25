62f4f974f840cd4060435e177c802659be31989a
Alex <alex.bul777@yandex.ru>
2023-04-26 00:52:56 +0300

    chart_test

--- virtual_rectification/Results.xaml.cs
+++ virtual_rectification/Results.xaml.cs
@@ -11,6 +11,19 @@ namespace virtual_rectification
         public Results()
         {
             InitializeComponent();
+            ChartBuilder();
         }
+
+        void ChartBuilder()
+        {
+            double[] dataX = new double[] { 1, 3, 6, 10, 30 };
+            double[] dataY = new double[] { 1, 4, 9, 16, 35 };
+            double[] dataX2 = new double[] { 1, 5, 11, 18, 30 };
+            double[] dataY2 = new double[] { 1, 6, 10, 15, 40 };
+            Chart.Plot.AddScatter(dataX, dataY);
+            Chart.Plot.AddScatter(dataX2, dataY2);
+            Chart.Refresh();
+        }
+
     }
 }
